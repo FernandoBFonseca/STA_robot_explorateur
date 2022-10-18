194eefead9ecb570265531eddbfb9f18b5542305
Fernando Fonseca <fernandobdf23@hotmail.com>
2022-10-18 20:07:58 +0200

    First test

--- Arduino/modeling_and_analyses/read_serial.py
+++ Arduino/modeling_and_analyses/read_serial.py
@@ -1,13 +1,30 @@
 import serial 
 import time 
+import pandas
+from datetime import datetime
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyS0', baudrate = 115200, timeout=1)
     ser.reset_input_buffer()
-
+    fp = open(f'test_{datetime.now()}.csv', 'a')
+    goBoy = False
+    
     while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(line)
+    
+        try:
+          isAvalible = ser.in_waiting > 0
+        except:
+            pass
+        
+        if isAvalible:
+              line = ser.readline().decode('utf-8').rstrip()
+              if goBoy:
+              
+                
+                fp.write(line)
+                print(line)
+                 
+              if 'boy' in line:
+                goBoy = True
             
 
